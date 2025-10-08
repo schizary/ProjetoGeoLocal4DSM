317da566ef7b7561fe2991ae8b5a6809368d1909
XandyGomes <x3.computadores@hotmail.com>
2025-10-07 21:19:28 -0300

    Aula.07.10.2025

---
+++ src/hooks/useLocation.js
@@ -0,0 +1,22 @@
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+
+export default function useLocation() {
+  const [coords, setCoords] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permissão negada para acesso a localização");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setCoords({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    })();
+  }, []);
+}
